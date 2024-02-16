e1d2a5949911a1dc698d6d9b3987a53f58eded3f
Alex Richards <alex.richards@AlexR-MB3625.local>
2024-02-16 11:00:16 -0500

    trying to fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col h-screen overflow-x-hidden`}>
-          <header className={'sticky'}>
-            <Header/>
-          </header>
-          <main className={'w-screen grow lg:flex lg:flex-row overflow-y-auto pb-16 lg:pb-0'}>
-            <Sidebar/>
-            <div className={'lg:w-screen lg:overflow-y-auto lg:grow'}>{children}</div>
-          </main>
+      <body className={`${inter.className} flex flex-col h-full overflow-x-hidden`}>
+        <Header/>
+        <main className={'w-screen grow lg:flex lg:flex-row overflow-y-auto pb-16 lg:pb-0'}>
+          <Sidebar/>
+          <div className={'lg:w-screen lg:overflow-y-auto lg:grow'}>{children}</div>
+        </main>
       </body>
     </html>
   );
