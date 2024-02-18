95a05ff4feda77da493e0266093ca220b7c1284d
Alex Richards <alex.richards@AlexR-MB3625.local>
2024-02-18 14:24:15 -0500

    stripping stuff down

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex h-screen bg-amber-500 overflow-y-hidden`}>
-        <main className={'flex w-screen bg-accent grow flex-row h-screen overflow-y-hidden'}>
+      <body className={`${inter.className} flex h-screen lg:flex-row bg-amber-500 overflow-y-hidden`}>
           <Header />
           <Sidebar/>
-          <div className={'lg:w-4/5 bg-gray-500 overflow-y-auto py-6'}>{children}</div>
-        </main>
+          <div className={'lg:w-4/5 bg-gray-500 overflow-y-auto'}>{children}</div>
       </body>
     </html>
   );
