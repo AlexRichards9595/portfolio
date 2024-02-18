013e1cdf1cd3d1d0e7f46a705e4622bd84466406
Alex Richards <alex.richards@AlexR-MB3625.local>
2024-02-18 14:03:58 -0500

    move header inside main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex h-screen bg-amber-500 overflow-y-hidden`}>
+        <main className={'flex w-screen bg-accent grow flex-row h-full overflow-y-auto lg:overflow-y-hidden'}>
         <Header />
-        <main className={'flex w-screen bg-accent grow flex-row h-screen overflow-y-auto lg:overflow-y-hidden'}>
           <Sidebar/>
           <div className={'lg:w-4/5 bg-gray-500 lg:overflow-y-auto overflow-x-hidden py-6'}>{children}</div>
         </main>
