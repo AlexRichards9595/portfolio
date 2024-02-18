391862ce29100ee2f87c30846109b75fc8f81f63
Alex Richards <alex.richards@AlexR-MB3625.local>
2024-02-18 14:14:20 -0500

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex h-screen bg-amber-500 overflow-y-hidden`}>
-        <main className={'flex w-screen bg-accent grow flex-row h-full overflow-y-auto lg:overflow-y-hidden'}>
-        <Header />
+        <main className={'flex w-screen bg-accent grow flex-row h-screen overflow-y-hidden'}>
+          <Header />
           <Sidebar/>
-          <div className={'lg:w-4/5 bg-gray-500 lg:overflow-y-auto overflow-x-hidden py-6'}>{children}</div>
+          <div className={'lg:w-4/5 bg-gray-500 overflow-y-auto py-6'}>{children}</div>
         </main>
       </body>
     </html>
